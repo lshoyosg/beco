fa54dc70cea41483593c64380f62b616c169f1de
lshoyosg <lshoyosg@unal.edu.co>
2025-09-07 15:06:14 -0500

    Agregé abrir_excel.py

---
+++ abrir_excel.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Ruta al archivo Excel
+excel_path = "DataBase/BECO_1975_2023_web.xlsx"
+
+
+# Cargar todas las hojas del archivo Excel en un diccionario de DataFrames
+all_sheets = pd.read_excel(excel_path, sheet_name=None)
+
+# Mostrar los nombres de las hojas y las primeras filas de cada una
+for sheet_name, df in all_sheets.items():
+	print(f"\nHoja: {sheet_name}")
+	print(df.head())
